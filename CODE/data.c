ff274d852e46eb7436a0ba76eb3013db750a80ec
ROY1994 <move_cloud_roy1994@qq.com>
2022-07-23 19:02:42 +0800

    save

--- CODE/data.c
+++ CODE/data.c
@@ -70,7 +70,7 @@ void data_set(void)
 
     // ================= 速度初始化设置 ================= //
 
-    ConstData.speed.kMaxSpeed = 150;
+    ConstData.speed.kMaxSpeed = 200;
     ConstData.speed.kNormalSpeed = 130;
     ConstData.speed.kCircleSpeed = 130;
     ConstData.speed.kPSpeed = 130;
